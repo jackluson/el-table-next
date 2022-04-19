46ea66fe733d8f7b7a8b2e5a66a4c88a28f13f25
jackluson <18219112108@163.com>
2022-04-19 18:22:41 +0800

    ci: update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,4 @@
-  
 #!/usr/bin/env sh
-
 set -e
 
 cd packages/el-table-next-docs
@@ -9,10 +7,17 @@ npm run docs:build
 cd docs/.vitepress/dist
 pwd
 
-git init
-git add -A
-git commit -m 'deploy'
+if [ ! -n "$1" ] ;then
+    echo "the deploy platform is github"
+    git init
+    git add -A
+    git commit -m 'deploy'
+    git push -f git@github.com:jackluson/el-table-next.git main:gh-pages
+else
+    echo "the deploy platform is $1"
+    # platform=$1
+    # echo -e "platform: $platform"
+fi
 
-git push -f git@github.com:jackluson/el-table-next.git main:gh-pages
+cd -
 
-# cd -
